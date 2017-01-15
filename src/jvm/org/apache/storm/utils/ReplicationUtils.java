f0f334d584fb7bcde4c2e5dfa26f9c4df4ce3a9d
Xunyun Liu <xunyunliu@gmail.com>
2017-01-15 20:31:51 +1100

    implemented load and save function

--- src/jvm/org/apache/storm/utils/ReplicationUtils.java
+++ src/jvm/org/apache/storm/utils/ReplicationUtils.java
@@ -8,6 +8,13 @@ public class ReplicationUtils {
 	public final static int TOPO_INITIALIZATION_TIME = 20;
 	public final static String TOPOLOGY_NUMREPLICATIONS = "topology.numreplications";
 
+
+	@SuppressWarnings("rawtypes")
+	public static boolean isRecovering(Map Stormconf) {
+		return getTopologyUpTime(Stormconf) >= TOPO_INITIALIZATION_TIME ? true : false;
+	}
+	
+
 	@SuppressWarnings("rawtypes")
 	public static long getTopologyUpTime(Map Stormconf) {
 		if (!Stormconf.containsKey(TOPO_START_TIME))
@@ -18,11 +25,6 @@ public class ReplicationUtils {
 
 		return uptime;
 
-	}
-
-	@SuppressWarnings("rawtypes")
-	public static boolean isRecovering(Map Stormconf) {
-		return getTopologyUpTime(Stormconf) >= TOPO_INITIALIZATION_TIME ? true : false;
 	}
 
 }
